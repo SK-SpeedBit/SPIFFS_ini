8ae771950c8b772267e39769c9c4f7ef497017d0
SpeedBit <33321284+SK-SpeedBit@users.noreply.github.com>
2019-04-25 23:27:55 +0200

    ver. 1.0

--- SPIFFS_ini.h
+++ SPIFFS_ini.h
@@ -1,3 +1,20 @@
+/*
+    ----------------------   SPIFFS_ini ver. 1.0  ----------------------
+      (c) 2019 SpeedBit, reg. Czestochowa, Poland
+    --------------------------------------------------------------------
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+
 #ifndef _SPIFFS_ini_
 #define _SPIFFS_ini_
 
